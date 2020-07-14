5278878a8ed4116166e88203d742663bf9066771
Edward Romero <legolamel@gmail.com>
2020-07-14 02:49:59 -0400

    fix

--- src/modules/tweeter.module.js
+++ src/modules/tweeter.module.js
@@ -2,33 +2,33 @@
  * Get's latest tweet by hitting tweeter api
  */
 async function getLatestTweet() {
-    return Promise.resolve({
-        "created_at": "Wed Oct 10 20:19:24 +0000 2018",
-        "id": 1050118621198921728,
-        "id_str": "1050118621198921728",
-        "text": "Sample tweeter call for tweety readme action",
-        "user": {},  
-        "entities": {}
-    })
+  return Promise.resolve({
+    created_at: "Wed Oct 10 20:19:24 +0000 2018",
+    id: 1050118621198921728,
+    id_str: "1050118621198921728",
+    text: "Sample tweeter call for tweety readme action",
+    user: {},
+    entities: {},
+  });
 }
 
 /**
- * Converts 
+ * Converts
  * tweet to markdown
- * @param {object} tweet 
+ * @param {object} tweet
  */
 async function convertTweetToMarkdown(tweet) {
-    return `<div style="width: 400px; height: 200px; border: solid 1px; border-radius: 50px">
+  return `<div style="width: 400px; height: 200px; border: solid 1px; border-radius: 50px">
     <div style="padding: 50px 30px">
     <img src="./tweeter.png" height=25>
     <span style="">${tweet.text}</span>
     </br>
-    <span style="font-size: 10px; text-decoration: none;">${created_at}</span>
+    <span style="font-size: 10px; text-decoration: none;">${tweet.created_at}</span>
     </div>
-    </div>`
+    </div>`;
 }
 
 module.exports = {
-    getLatestTweet,
-    convertTweetToMarkdown,
-}
+  getLatestTweet,
+  convertTweetToMarkdown,
+};
